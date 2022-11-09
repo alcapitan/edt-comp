2da1cc26c4c64f26a6bc4fca0f173e62d6d20084
Al Capitan <alexandre.boyer29@gmail.com>
2022-11-09 09:49:22 +0100

    Travail du 09/11
    Surtout du nettoyage de code.

--- main.py
+++ main.py
@@ -5,33 +5,36 @@ class Person():
         self.timetable = {}
         self.name = None
         for i in range(7):
-            self.timetable[i] = [None for j in range(7)]
+            self.timetable[i] = [None for j in range(4)]
+    
+    def changeName(self,newName):
+        self.name = newName
     
     def generate(self,timetable):
-        # self.changeName(timetable.name) à changer !!!!!!
-        for i in range(len(timetable)):
-            print(timetable.keys())
-            if i in timetable.keys():
-                self.generateDay(i,timetable[i])
+        for day in self.timetable.keys():
+            if day in timetable.keys():
+                self.generateDay(day,timetable[day])
             else:
-                self.generateDay(i,[Null for j in range(4)])
+                self.generateDay(day,[None for i in range(4)])
     
     def generateDay(self,day,hours):
-        for i in range(7):
+        for i in range(4):
             self.generateHour(day,i,hours[i])
     
     def generateHour(self,day,moment,hour):
         self.timetable[day][moment] = hour
-    
-    def changeName(self,newName):
-        self.name = newName
 
 
 def display(person):
-    dayName = {0:'lundi',1:'mardi',2:'mecredi',3:'jeudi',4:'vendredi',5:'samedi',6:'dimanche'}
+    dayName = {0:'lundi',1:'mardi',2:'mercredi',3:'jeudi',4:'vendredi',5:'samedi',6:'dimanche'}
     print(person.name.upper())
     for day in person.timetable:
-        print(f"{dayName[day].capitalize()} : ")
+        courseMorning = not((person.timetable[0] == None) and (person.timetable[1] == None))
+        courseAfternoon = not((person.timetable[2] == None) and (person.timetable[3] == None))
+        if courseMorning and courseAfternoon:
+            print("Pas cours ce jour.")
+        else:
+            print(f"{dayName[day].capitalize()} : {person.timetable[day]}")
     return
 
 """currentTime = datetime.now().strftime("%H:%M")
